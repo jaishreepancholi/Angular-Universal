b6bb2bc9b2d9f012ddc24481d3b17814af6767b4
jasihree pancholi <jaishree.pancholi@ranosys.com>
2018-10-23 17:24:24 +0530

    Angular universal

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Angular-Universal
+# Angular 4+ SEO Starter Template
+
+This project is based on the [Angular SEO Tutorial](https://coursetro.com/posts/code/68/Make-your-Angular-App-SEO-Friendly-(Angular-4-+-Universal)) found at [Coursetro.com](https://coursetro.com)
+
+Feel free to clone this repo as a quick way to get up and running with an SEO-ready, Angular CLI-ready, Angular 4+ project.
+
+Simply clone the repo, run `npm install` in the project directory, then `npm run start`
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'ang4-seo'}),
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
